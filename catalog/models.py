95d922b981bc9ae50078ab829885303f19f2dc97
Prodeon <prodeon21@gmail.com>
2026-02-07 12:00:31 +0300

    Изменения моделей под требования ТЗ

--- catalog/models.py
+++ catalog/models.py
@@ -2,19 +2,24 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150)
-    description = models.TextField()
+    name = models.CharField(max_length=150, verbose_name="название")
+    description = models.TextField(verbose_name="описание")
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
 
 class Product(models.Model):
 
-    name = models.CharField(max_length=150)
-    description = models.TextField()
-    image = models.ImageField(upload_to='/static/images/')
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
-    price = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=150, verbose_name="наименование")
+    description = models.TextField(verbose_name="описание")
+    image = models.ImageField(upload_to='proudcts/', verbose_name="изображение",
+                              blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    price = models.FloatField(verbose_name="цена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="дата обновления")
 
     def __str__(self):
         return f"{self.name} {self.price}"
--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 from os import getenv
+
 
 from dotenv import load_dotenv
 load_dotenv(override=True)
@@ -129,3 +131,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
