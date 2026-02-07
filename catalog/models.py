66a30411605a71964aa9e45167a8c0718275403f
Prodeon <prodeon21@gmail.com>
2026-02-07 11:39:15 +0300

    Добавлены модели категорий и продуктов

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    image = models.ImageField(upload_to='/static/images/')
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} {self.price}"
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ["price"]
