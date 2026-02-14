5377da54eb09c2634f2d1c85de8494ddc9f15f21
Prodeon <prodeon21@gmail.com>
2026-02-14 10:41:15 +0300

    done with product detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,6 @@ app_name = "catalog"
 
 urlpatterns = [
     path("", views.home_page, name="home"),
-    path("contacts/", views.contact, name="contacts")
+    path("contacts/", views.contact, name="contacts"),
+    path("product/<int:pk>/", views.one_product, name="product")
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Category, Product
 
 # Create your views here.
 def home_page(request):
@@ -10,5 +11,11 @@ def contact(request):
     if request.method == "POST":
         return HttpResponse("<div><h1>Данные успешно отправлены</h1></>")
     return render(request, "contact/contacts.html")
+
+
+def one_product(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {"product" : product}
+    return render(request, "product/product.html", context=context)
 
 
