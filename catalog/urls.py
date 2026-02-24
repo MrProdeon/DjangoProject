0f481ece0a0f6701a2529d6c0adeff84ee704ba8
Prodeon <prodeon21@gmail.com>
2026-02-24 08:10:57 +0300

    views from FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "catalog"
 
 urlpatterns = [
-    path("", views.home_page, name="home"),
-    path("contacts/", views.contact, name="contacts"),
-    path("product/<int:pk>/", views.one_product, name="product"),
-    path("add_product/", views.add_product, name="add_product")
+    path("", views.ProductListView.as_view(), name="home"),
+    path("contacts/", views.Contact.as_view(), name="contacts"),
+    path("product/<int:pk>/", views.ProductDetailView.as_view(), name="product"),
+    path("add_product/", views.ProductCreateView.as_view(), name="add_product")
 ]
