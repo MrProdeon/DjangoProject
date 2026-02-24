0161f5d84bc9e78bebf01a7b6f24f4a2432f0294
Prodeon <prodeon21@gmail.com>
2026-02-24 08:19:43 +0300

    create model BlogEntry for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BlogEntry(models.Model):
+
+    name = models.CharField(max_length=150)
+    context = models.TextField()
+    preview = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_publicated = models.BooleanField()
+    views_count = models.IntegerField()
+    
