b4aa0367f561f09e9cb3a6acf783b1f9cb2feb43
Prodeon <prodeon21@gmail.com>
2026-02-26 16:35:25 +0300

    add view and template for detail read

--- blog/views.py
+++ blog/views.py
@@ -18,6 +18,11 @@ class BlogListView(ListView):
     template_name = "home.html"
     context_object_name = "posts"
 
+class BlogDetailView(DetailView):
+    model = BlogEntry
+    template_name = "blog_detail.html"
+    context_object_name = "post"
+
 class BlogUpdateView(UpdateView):
     model = BlogEntry
     fields = ["name", "content", "preview"]
