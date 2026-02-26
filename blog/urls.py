2ce24be3fcff0c77937f00e619e3df225977b8cc
Prodeon <prodeon21@gmail.com>
2026-02-26 11:56:43 +0300

    fix blogs urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name = "blog"
+app_name = "blogs"
 
 urlpatterns = [
     path("", views.BlogListView.as_view(), name="home"),
