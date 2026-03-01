30d3c302b1bd62620f95c6e891d07fa7eeca96e3
Prodeon <prodeon21@gmail.com>
2026-03-01 11:13:03 +0300

    add urls for update_product

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("", views.ProductListView.as_view(), name="home"),
     path("contacts/", views.Contact.as_view(), name="contacts"),
     path("product/<int:pk>/", views.ProductDetailView.as_view(), name="product"),
-    path("add_product/", views.ProductCreateView.as_view(), name="add_product")
+    path("add_product/", views.ProductCreateView.as_view(), name="add_product"),
+    path("update_product/<int:pk>/", views.ProductUpdateView.as_view(), name="update_product")
 ]
