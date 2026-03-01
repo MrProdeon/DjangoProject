f326005cd29120e54b0128f880eee88a9a9e963e
Prodeon <prodeon21@gmail.com>
2026-03-01 12:33:05 +0300

    add url for delete product

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("contacts/", views.Contact.as_view(), name="contacts"),
     path("product/<int:pk>/", views.ProductDetailView.as_view(), name="product"),
     path("add_product/", views.ProductCreateView.as_view(), name="add_product"),
-    path("update_product/<int:pk>/", views.ProductUpdateView.as_view(), name="update_product")
+    path("update_product/<int:pk>/", views.ProductUpdateView.as_view(), name="update_product"),
+    path("delete_product/<int:pk>/", views.ProductDeleteeView.as_view(), name="delete_product")
 ]
