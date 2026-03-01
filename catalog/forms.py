4409dd9e7e987fcc3380149d534bbff4c1f840e5
Prodeon <prodeon21@gmail.com>
2026-03-01 13:11:07 +0300

    separation of validation by fields

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,6 +3,11 @@ from catalog.models import Product
 from django.core.exceptions import ValidationError
 
 class ProductForm(forms.ModelForm):
+
+    ban_words = ["казино", "криптовалюта", "крипта",
+                 "биржа", "дешево", "бесплатно",
+                 "обман", "полиция", "радар"]
+
     class Meta:
         model = Product
         fields = ["name", "description", "category", "price", "image"]
@@ -25,27 +30,44 @@ class ProductForm(forms.ModelForm):
 
         return price
 
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
 
-    def clean(self):
-        cleaned_data = super().clean()
-        name = cleaned_data.get("name")
-        description = cleaned_data.get("description")
+        is_name_in_ban = [word for word in self.ban_words if word in name.lower()]
 
-        ban_words = ["казино", "криптовалюта", "крипта",
-                     "биржа", "дешево", "бесплатно",
-                     "обман", "полиция", "радар"]
+        if is_name_in_ban:
+            raise ValidationError(f'Имя не может содержать слово: {", ".join(is_name_in_ban)}')
 
-        is_name_in_ban = [word for word in ban_words if word in name.lower()]
-        is_description_in_ban = [word for word in ban_words if word in description.lower()]
+        return name
 
-        if name and description and any((is_name_in_ban, is_description_in_ban)):
+    def clean_description(self):
+        name = self.cleaned_data.get("description")
 
-            if is_name_in_ban:
-                self.add_error("name", f'Имя не может содержать слово: {", ".join(is_name_in_ban)}')
-            if is_description_in_ban:
-                self.add_error("description", f'Имя не может содержать слово {", ".join(is_description_in_ban)}')
+        is_description_in_ban = [word for word in self.ban_words if word in name.lower()]
 
-        return cleaned_data
+        if is_description_in_ban:
+            raise ValidationError(f'Описание не может содержать слово: {", ".join(is_description_in_ban)}')
+
+        return description
+
+
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     name = cleaned_data.get("name")
+    #     description = cleaned_data.get("description")
+    #
+    #     is_name_in_ban = [word for word in ban_words if word in name.lower()]
+    #     is_description_in_ban = [word for word in ban_words if word in description.lower()]
+    #
+    #     if name and description and any((is_name_in_ban, is_description_in_ban)):
+    #
+    #         if is_name_in_ban:
+    #             self.add_error("name", f'Имя не может содержать слово: {", ".join(is_name_in_ban)}')
+    #         if is_description_in_ban:
+    #             self.add_error("description", f'Имя не может содержать слово {", ".join(is_description_in_ban)}')
+    #
+    #     return cleaned_data
 
 class ProductDeleteForm(forms.Form):
     pass
