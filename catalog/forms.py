854f2c8dd7a94f8ded53bfa85bfb2cd85a16e31e
Prodeon <prodeon21@gmail.com>
2026-03-01 11:17:28 +0300

    add form for update and create product

---
+++ catalog/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from catalog.models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ["name", "description", "category", "price", "image"]
