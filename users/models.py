e61230d93ac0bfe7f84cc9b3ffab860c7ec90cf4
Prodeon <prodeon21@gmail.com>
2026-03-06 07:16:13 +0300

    add username, email and required_fields

--- users/models.py
+++ users/models.py
@@ -8,7 +8,11 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15)
     country = models.CharField(max_length=100)
 
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email address')
+
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['phone_number', 'country']
 
     def __str__(self):
         return self.email
