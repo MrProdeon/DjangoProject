8931978f45d9e8216b02b238f382d4d5529b21d4
Prodeon <prodeon21@gmail.com>
2026-03-06 07:13:52 +0300

    add RegisterView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from users.forms import CustomUserCreationForms
+from django.urls import reverse_lazy
 
-# Create your views here.
+class RegisterView(View):
+    form_class = CustomUserCreationForms
+    template_name = "register.html"
+    success_url = reverse_lazy("catalog:home")
