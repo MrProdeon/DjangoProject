adabeec4bad5f2a4dbd23d1e7d18d02d16a3a38f
Prodeon <prodeon21@gmail.com>
2026-03-07 08:43:32 +0300

    add form_valid and self_welcome_email methods in register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.shortcuts import render
 from django.views.generic import CreateView
 from users.forms import CustomUserCreationForms
@@ -9,3 +10,17 @@ class RegisterView(CreateView):
     form_class = CustomUserCreationForms
     template_name = "register.html"
     success_url = reverse_lazy("catalog:home")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    @staticmethod
+    def send_welcome_email(self, user_email):
+        subject = "Добро пожаловать на наш сайт!"
+        message = "Благодарим за регистрацию!"
+        from_email = "" # NEED TO CREATE
+        recipient_list = [user_email]
+        send_mail(subject, message, recipient_list, from_email)
