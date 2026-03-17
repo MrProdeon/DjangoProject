45b67a04a6940d5318c0b9d65be9907b9b7fdb98
Prodeon <prodeon21@gmail.com>
2026-03-17 11:50:18 +0300

    add custom perms in model Product

--- catalog/models.py
+++ catalog/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="дата обновления")
     is_in_stock = models.BooleanField(default=True, verbose_name="В наличии")
+    is_publicated = models.BooleanField(default=False, verbose_name="Статус публикации")
 
     def __str__(self):
         return f"{self.name} {self.price}"
@@ -32,6 +33,9 @@ class Product(models.Model):
         verbose_name = "продукт"
         verbose_name_plural = "продукты"
         ordering = ["price"]
+        permissions = [
+            ("can_unpublish_product", "Can unpublish product")
+        ]
 
 class Contacts(models.Model):
 
