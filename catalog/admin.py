532ff10be973be94826f1b2559a9556aaeda2dd8
Prodeon <prodeon21@gmail.com>
2026-03-17 13:31:40 +0300

    add owner in list display

--- catalog/admin.py
+++ catalog/admin.py
@@ -11,7 +11,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "category", "description", "is_publicated")
+    list_display = ("id", "name", "price", "category", "description", "is_publicated", "owner")
     list_filter = ("category",)
     search_fields = ("name", "description")
 
