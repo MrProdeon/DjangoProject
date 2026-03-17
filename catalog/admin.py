fef1ffc53a7403e50865e0b8ed2c1f7e6761e6f9
Prodeon <prodeon21@gmail.com>
2026-03-17 12:31:21 +0300

    add user model in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from catalog.models import Category, Product, Contacts
+from users.models import CustomUser
 
 
 @admin.register(Category)
@@ -20,3 +21,7 @@ class ContactsAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "number")
     list_filter = ("id", "name")
     search_fields = ("name", "number")
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
